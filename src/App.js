60af67cc53896a34197a375d9d48a1db08dd9512
Aiswaryalakshmics <aiswaryalakshmi48@gmail.com>
2023-10-27 22:16:10 +0530

    backgroundcolor changer app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [backgroundColor,setBackgroundColor] = useState('#ee90ed');
+  const changeBackgroundColor = (color) =>{
+    setBackgroundColor(color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{width:'100%',height:'100vh',display:'flex',flexDirection: 'column',justifyContent:'center',
+      alignItems:'center',textAlign:'center',backgroundColor: backgroundColor}}>
+        <div style={{fontSize:'50px',fontWeight:'bold'}}>Background Color Change</div>
+        <div className='d-flex mt-5'>
+          <Button onClick={()=>changeBackgroundColor('red')} style={{borderRadius:'50%'}} variant="light">RED</Button>
+          <Button onClick={()=>changeBackgroundColor('blue')} style={{borderRadius:'50%'}} className='ms-3' variant="light">BLUE</Button>
+          <Button onClick={()=>changeBackgroundColor('#0FFF50')} style={{borderRadius:'50%'}} className='ms-3' variant="light">GREEN</Button>
+          <Button onClick={()=>changeBackgroundColor('yellow')} style={{borderRadius:'50%'}} className=' ms-3' variant="light">YELLOW</Button>  
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
